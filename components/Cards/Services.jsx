aa4c344b68a41032927b147b2fcb1cdb99c651fb
Azamat <azamatduishenkulov711@gmail.com>
2022-06-16 16:44:58 +0600

    Add

---
+++ components/Cards/Services.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from "./Services.module.sass"
+
+export default function Services() {
+    return (
+    <div className={styles.container}>
+        <h1 className={styles.h1__servise}>servise</h1>
+    </div>
+    )
+}
