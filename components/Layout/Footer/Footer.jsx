e70f7e26c204a2111b30a76b3fbe82d77f29ae8d
Melis <tyn.melis.ru@gmail.com>
2022-06-16 18:29:55 +0300

    fix

--- components/Layout/Footer/Footer.jsx
+++ components/Layout/Footer/Footer.jsx
@@ -2,9 +2,6 @@ import React from "react";
 import styles from "./Footer.module.sass";
 
 export default function Footer() {
-<<<<<<< HEAD
-  return <div></div>;
-=======
   return (
     <header className={styles.footer}>
       <div className={styles.container}>
@@ -15,5 +12,4 @@ export default function Footer() {
       </div>
     </header>
   );
->>>>>>> 28b69d89b7da519a155a9c1609f8b58e7bb260f6
 }
