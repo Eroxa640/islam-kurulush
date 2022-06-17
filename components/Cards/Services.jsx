edbc89d189bee85ef01cd46641fe4970ccc2f4df
Erbol <a1darbekov640@gmail.com>
2022-06-17 17:41:19 +0300

    add service

--- components/Cards/Services.jsx
+++ components/Cards/Services.jsx
@@ -3,38 +3,60 @@ import styles from "./Services.module.sass"
 
 export default function Services() {
     return (
-    <div className={styles.bg}>
-      <div className={styles.container}>
-        <div className={styles.erbol}>
-          <div className={styles.flex}>
-            <h5 className={styles.h5__service}>Наши услуги</h5>
-            <h1 className={styles.h1__service}>Мы специализируемся</h1>
+    <div className={styles.container}>
+      <div className={styles.flex}>
+        <div className={styles.about}>
+          <p className={styles.about__uslugi}>Наши услуги</p>
+          <h3 className={styles.about__title}>Мы специализируемся</h3>
+        </div>
+        <div className={styles.cards}>
+          <div className={styles.card}>
+            <div className={styles.card__bg}>
+              <img src="images/img-services.png" alt="Строительстве нового поколения жилых домов" className={styles.card__image} />
+            </div>
+            <div>
+              <h3 className={styles.card__title}>Строительстве нового поколения жилых домов</h3>
+            </div>
           </div>
-          <div className={styles.cards}>
-            <div className={styles.card}>
-              <img src="images/img-services.png" alt="" className={styles.img__service} />
-              <h2 className={styles.h2__service}>Строительстве нового поколения жилых домов</h2>
+          <div className={styles.card}>
+            <div className={styles.card__bg}>
+              <img src="images/img-services.png" alt="Строительстве нового поколения жилых домов" className={styles.card__image} />
             </div>
-            <div className={styles.card}>
-              <img src="images/img-services.png" alt="" className={`${styles.img__service} ${styles.img__srv}`} />
-              <h2 className={`${styles.h2__service} ${styles.h2__srv}`}>Строительстве нового поколения жилых домов</h2>
+            <div>
+              <h3 className={styles.card__title}>Строительстве нового поколения жилых домов</h3>
             </div>
-            <div className={styles.card}>
-              <img src="images/img-services.png" alt="" className={`${styles.img__service} ${styles.img__srv}`} />
-              <h2 className={`${styles.h2__service} ${styles.h2__srv}`}>Строительстве нового поколения жилых домов</h2>
+          </div>
+          <div className={styles.card}>
+            <div className={styles.card__bg}>
+              <img src="images/img-services.png" alt="Строительстве нового поколения жилых домов" className={styles.card__image} />
             </div>
-            <div className={styles.card}>
-              <img src="images/img-services.png" alt="" className={`${styles.img__service} ${styles.img__srv}`} />
-              <h2 className={`${styles.h2__service} ${styles.h2__srv}`}>Строительстве нового поколения жилых домов</h2>
+            <div>
+              <h3 className={styles.card__title}>Строительстве нового поколения жилых домов</h3>
             </div>
-            <div className={styles.card}>
-              <img src="images/img-services.png" alt="" className={`${styles.img__service} ${styles.img__srv}`} />
-              <h2 className={`${styles.h2__service} ${styles.h2__srv}`}>Строительстве нового поколения жилых домов</h2>
+          </div>
+          <div className={styles.card}>
+            <div className={styles.card__bg}>
+              <img src="images/img-services.png" alt="Строительстве нового поколения жилых домов" className={styles.card__image} />
             </div>
-            <div className={styles.card}>
-              <img src="images/img-services.png" alt="" className={`${styles.img__service} ${styles.img__srv}`}/>
-              <h2 className={`${styles.h2__service} ${styles.h2__srv}`}>Строительстве нового поколения жилых домов</h2>
-            </div>         
+            <div>
+              <h3 className={styles.card__title}>Строительстве нового поколения жилых домов</h3>
+            </div>
+          </div>
+          <div className={styles.card}>
+            <div className={styles.card__bg}>
+              <img src="images/img-services.png" alt="Строительстве нового поколения жилых домов" className={styles.card__image} />
+            </div>
+            <div>
+              <h3 className={styles.card__title}>Строительстве нового поколения жилых домов</h3>
+            </div>
+          </div>
+          <div className={styles.card}>
+            <div className={styles.card__bg}>
+              <img src="images/img-services.png" alt="Строительстве нового поколения жилых домов" className={styles.card__image} />
+            </div>
+            <div>
+              <h3 className={styles.card__title}>Строительстве нового поколения жилых домов</h3>
+            </div>
           </div>
         </div>
       </div>
