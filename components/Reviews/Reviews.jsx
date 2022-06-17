939e224b62ddd33ede5b2a4f68fb7595c9b5fcff
Melis <tyn.melis.ru@gmail.com>
2022-06-17 15:40:54 +0300

    add pagination

--- components/Reviews/Reviews.jsx
+++ components/Reviews/Reviews.jsx
@@ -1,7 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Reviews.module.sass";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination, Navigation } from "swiper";
+import "swiper/css/navigation";
+import "swiper/css";
+import "swiper/css/pagination";
 
 export default function Reviews() {
+  const navigationPrevRef = React.useRef(null);
+  const navigationNextRef = React.useRef(null);
   return (
     <div className={styles.reviews}>
       <div className={styles.reviews_container}>
@@ -9,19 +16,74 @@ export default function Reviews() {
           <h4 className={styles.reviews_title}>Отзывы</h4>
           <h2 className={styles.reviews_thanks}>Ваши благодарности</h2>
         </div>
-        <div className={styles.grey_block}>
-          <h2 className={styles.grey_description}>
-            Спасибо вам за качествунную квартиру. Живем более 3х лет на квартире
-            которая построила компания "Ислам Курулуш"{" "}
-          </h2>
-          <div className={styles.photo_block}>
-            <img src="images/Gulmira-Asanova.png" alt="Гульмира Асанова" />
-            <div className={styles.photo_description}>
-              <h3 className={styles.photo_h3}>Гульмира Асанова</h3>
-              <h5 className={styles.photo_h5}>Домохозяйка</h5>
+        <Swiper
+          containerModifierClass="slider-"
+          pagination={{
+            type: "bullets",
+            horizontalClass: styles.swiperPaginationHorizontal,
+            modifierClass: styles.swiperPagination,
+            clickable: true,
+          }}
+          navigation={{
+            prevEl: navigationPrevRef.current,
+            nextEl: navigationNextRef.current,
+          }}
+          className="slider"
+          modules={[Pagination, Navigation]}
+        >
+          <SwiperSlide className={styles.grey_block}>
+            <h2 className={styles.grey_description}>
+              Спасибо вам за качествунную квартиру. Живем более 3х лет на
+              квартире которая построила компания "Ислам Курулуш"{" "}
+            </h2>
+            <div className={styles.photo_block}>
+              <img src="images/Gulmira-Asanova.png" alt="Гульмира Асанова" />
+              <div className={styles.photo_description}>
+                <h3 className={styles.photo_h3}>Гульмира Асанова</h3>
+                <h5 className={styles.photo_h5}>Домохозяйка</h5>
+              </div>
             </div>
-          </div>
-        </div>
+          </SwiperSlide>
+          <SwiperSlide className={styles.grey_block}>
+            <h2 className={styles.grey_description}>
+              Спасибо вам за качествунную квартиру. Живем более 3х лет на
+              квартире которая построила компания "Ислам Курулуш"{" "}
+            </h2>
+            <div className={styles.photo_block}>
+              <img src="images/Gulmira-Asanova.png" alt="Гульмира Асанова" />
+              <div className={styles.photo_description}>
+                <h3 className={styles.photo_h3}>Гульмира Асанова</h3>
+                <h5 className={styles.photo_h5}>Домохозяйка</h5>
+              </div>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide className={styles.grey_block}>
+            <h2 className={styles.grey_description}>
+              Спасибо вам за качествунную квартиру. Живем более 3х лет на
+              квартире которая построила компания "Ислам Курулуш"{" "}
+            </h2>
+            <div className={styles.photo_block}>
+              <img src="images/Gulmira-Asanova.png" alt="Гульмира Асанова" />
+              <div className={styles.photo_description}>
+                <h3 className={styles.photo_h3}>Гульмира Асанова</h3>
+                <h5 className={styles.photo_h5}>Домохозяйка</h5>
+              </div>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide className={styles.grey_block}>
+            <h2 className={styles.grey_description}>
+              Спасибо вам за качествунную квартиру. Живем более 3х лет на
+              квартире которая построила компания "Ислам Курулуш"{" "}
+            </h2>
+            <div className={styles.photo_block}>
+              <img src="images/Gulmira-Asanova.png" alt="Гульмира Асанова" />
+              <div className={styles.photo_description}>
+                <h3 className={styles.photo_h3}>Гульмира Асанова</h3>
+                <h5 className={styles.photo_h5}>Домохозяйка</h5>
+              </div>
+            </div>
+          </SwiperSlide>
+        </Swiper>
       </div>
     </div>
   );
