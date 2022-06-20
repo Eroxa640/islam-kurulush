c5d362d0614b2d22601cdeb6e287ac2f4da720bd
Melis <tyn.melis.ru@gmail.com>
2022-06-20 15:44:04 +0300

    fix navbar margin

--- data/contact.js
+++ data/contact.js
@@ -2,22 +2,22 @@ const contact = [
   {
     title: "+996 (700) 556 030",
     link: "phone.png",
-    id: 1,
+    id: 5,
   },
   {
     title: "Пн-Пт 10:00-18:00",
     link: "time.png",
-    id: 2,
+    id: 6,
   },
   {
     title: "islam-kurulush.kg",
     link: "email.png",
-    id: 3,
+    id: 7,
   },
   {
     title: "Мкр Джал-29",
     link: "location.png",
-    id: 4,
+    id: 8,
   },
 ];
 export default contact;
