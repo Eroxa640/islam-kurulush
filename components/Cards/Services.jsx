90d2d8692e84e15ab988b21a33f17b260365281a
Melis <tyn.melis.ru@gmail.com>
2022-06-21 14:39:37 +0300

    framer motion

--- components/Cards/Services.jsx
+++ components/Cards/Services.jsx
@@ -5,6 +5,7 @@ import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/effect-cards";
 import { EffectCards, Pagination } from "swiper";
+import { motion, AnimatePresence } from "framer-motion";
 
 export default function Services() {
   const [screenWidth, setScreenWidth] = useState(false);
@@ -21,13 +22,79 @@ export default function Services() {
     window.addEventListener("resize", handleWidth);
     return () => window.removeEventListener("resize", handleWidth);
   }, []);
+
+  //motion
+  let firstTeamVar = {
+    hidden: {
+      x: -40,
+      opacity: 0,
+    },
+    visible: (num) => ({
+      x: 0,
+      opacity: 1,
+      transition: {
+        duration: 0.5,
+        delay: num * 0.1,
+      },
+    }),
+  };
+  let secondTeamVar = {
+    hidden: {
+      x: 40,
+      opacity: 0,
+    },
+    visible: (num) => ({
+      x: 0,
+      opacity: 1,
+      transition: {
+        duration: 0.5,
+        delay: num * 0.1,
+      },
+    }),
+  };
+  let hiddenVar = {
+    hidden: {
+      opacity: 0,
+    },
+    visible: (num) => ({
+      opacity: 1,
+      transition: {
+        duration: 0.5,
+        delay: num * 0.1,
+      },
+    }),
+  };
   return (
     <div className={styles.cards__container}>
       <div className={styles.container}>
         <div className={styles.flex}>
           <div className={styles.about}>
-            <p className={styles.about__uslugi}>Наши услуги</p>
-            <h3 className={styles.about__title}>Мы специализируемся</h3>
+            <motion.p
+              className={styles.about__uslugi}
+              initial="hidden"
+              whileInView="visible"
+              viewport={{
+                once: true,
+                amount: 1,
+              }}
+              variants={firstTeamVar}
+              custom={1}
+            >
+              Наши услуги
+            </motion.p>
+            <motion.h3
+              className={styles.about__title}
+              initial="hidden"
+              whileInView="visible"
+              viewport={{
+                once: true,
+                amount: 1,
+              }}
+              variants={firstTeamVar}
+              custom={1}
+            >
+              Мы специализируемся
+            </motion.h3>
           </div>
           {screenWidth ? (
             <Swiper
@@ -113,7 +180,17 @@ export default function Services() {
             </Swiper>
           ) : (
             <div className={styles.cards}>
-              <div className={styles.card__bg}>
+              <motion.div
+                className={styles.card__bg}
+                initial="hidden"
+                whileInView="visible"
+                viewport={{
+                  once: true,
+                  amount: 0.5,
+                }}
+                variants={secondTeamVar}
+                custom={1}
+              >
                 <img
                   src="images/img-services.png"
                   alt="Строительстве нового поколения жилых домов"
@@ -124,8 +201,18 @@ export default function Services() {
                     Строительстве нового поколения жилых домов
                   </h3>
                 </div>
-              </div>
-              <div className={styles.card__bg}>
+              </motion.div>
+              <motion.div
+                className={styles.card__bg}
+                initial="hidden"
+                whileInView="visible"
+                viewport={{
+                  once: true,
+                  amount: 0.5,
+                }}
+                variants={secondTeamVar}
+                custom={2}
+              >
                 <img
                   src="images/img-services.png"
                   alt="Строительстве нового поколения жилых домов"
@@ -136,8 +223,18 @@ export default function Services() {
                     Строительстве нового поколения жилых домов
                   </h3>
                 </div>
-              </div>
-              <div className={styles.card__bg}>
+              </motion.div>
+              <motion.div
+                className={styles.card__bg}
+                initial="hidden"
+                whileInView="visible"
+                viewport={{
+                  once: true,
+                  amount: 0.5,
+                }}
+                variants={secondTeamVar}
+                custom={3}
+              >
                 <img
                   src="images/img-services.png"
                   alt="Строительстве нового поколения жилых домов"
@@ -148,8 +245,18 @@ export default function Services() {
                     Строительстве нового поколения жилых домов
                   </h3>
                 </div>
-              </div>
-              <div className={styles.card__bg}>
+              </motion.div>
+              <motion.div
+                className={styles.card__bg}
+                initial="hidden"
+                whileInView="visible"
+                viewport={{
+                  once: true,
+                  amount: 0.5,
+                }}
+                variants={secondTeamVar}
+                custom={4}
+              >
                 <img
                   src="images/img-services.png"
                   alt="Строительстве нового поколения жилых домов"
@@ -160,8 +267,18 @@ export default function Services() {
                     Строительстве нового поколения жилых домов
                   </h3>
                 </div>
-              </div>
-              <div className={styles.card__bg}>
+              </motion.div>
+              <motion.div
+                className={styles.card__bg}
+                initial="hidden"
+                whileInView="visible"
+                viewport={{
+                  once: true,
+                  amount: 0.5,
+                }}
+                variants={secondTeamVar}
+                custom={5}
+              >
                 <img
                   src="images/img-services.png"
                   alt="Строительстве нового поколения жилых домов"
@@ -172,8 +289,18 @@ export default function Services() {
                     Строительстве нового поколения жилых домов
                   </h3>
                 </div>
-              </div>
-              <div className={styles.card__bg}>
+              </motion.div>
+              <motion.div
+                className={styles.card__bg}
+                initial="hidden"
+                whileInView="visible"
+                viewport={{
+                  once: true,
+                  amount: 0.5,
+                }}
+                variants={secondTeamVar}
+                custom={6}
+              >
                 <img
                   src="images/img-services.png"
                   alt="Строительстве нового поколения жилых домов"
@@ -184,7 +311,7 @@ export default function Services() {
                     Строительстве нового поколения жилых домов
                   </h3>
                 </div>
-              </div>
+              </motion.div>
             </div>
           )}
         </div>
--- components/MainHead/MainHead.jsx
+++ components/MainHead/MainHead.jsx
@@ -1,14 +1,83 @@
 import React from "react";
 import styles from "./Head.module.sass";
+import { motion, AnimatePresence } from "framer-motion";
 
 export default function MainHead() {
+  let firstTeamVar = {
+    hidden: {
+      x: -40,
+      opacity: 0,
+    },
+    visible: (num) => ({
+      x: 0,
+      opacity: 1,
+      transition: {
+        duration: 0.5,
+        delay: num * 0.1,
+      },
+    }),
+  };
+  let secondTeamVar = {
+    hidden: {
+      x: 40,
+      opacity: 0,
+    },
+    visible: (num) => ({
+      x: 0,
+      opacity: 1,
+      transition: {
+        duration: 0.5,
+        delay: num * 0.1,
+      },
+    }),
+  };
   return (
     <div className={styles.main__header}>
       <div className={styles.container}>
         <div className={styles.block}>
-          <h3 className={styles.companyNameOne}>Компания</h3>
-          <h3 className={styles.companyName}>Ислам Курулуш</h3>
-          <p className={styles.parag}>Cоздание и мастерство на всех этапах строительных и монтажных работ, внимание к деталям и особым пожелания заказчиков. Умение совмещать несовместимое, находить и нестандартные и эффективные методы решения текущих задач, предлагать лучшие варианты реализации проектов в заданных условиях-умения которое дано не каждому</p>
+          <motion.h3
+            className={styles.companyNameOne}
+            initial="hidden"
+            whileInView="visible"
+            viewport={{
+              once: true,
+              amount: 1,
+            }}
+            variants={firstTeamVar}
+            custom={1}
+          >
+            Компания
+          </motion.h3>
+          <motion.h3
+            className={styles.companyName}
+            initial="hidden"
+            whileInView="visible"
+            viewport={{
+              once: true,
+              amount: 1,
+            }}
+            variants={firstTeamVar}
+            custom={2}
+          >
+            Ислам Курулуш
+          </motion.h3>
+          <motion.p
+            className={styles.parag}
+            initial="hidden"
+            whileInView="visible"
+            viewport={{
+              once: true,
+              amount: 1,
+            }}
+            variants={secondTeamVar}
+            custom={1}
+          >
+            Cоздание и мастерство на всех этапах строительных и монтажных работ,
+            внимание к деталям и особым пожелания заказчиков. Умение совмещать
+            несовместимое, находить и нестандартные и эффективные методы решения
+            текущих задач, предлагать лучшие варианты реализации проектов в
+            заданных условиях-умения которое дано не каждому
+          </motion.p>
         </div>
       </div>
       <div className={styles.main}>
