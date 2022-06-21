01a6f0a1c9ed78422b44e99e9930cd6e3256fe8c
Erbol <a1darbekov640@gmail.com>
2022-06-21 17:25:11 +0300

    Add Framer motion About and service

--- pages/About/index.jsx
+++ pages/About/index.jsx
@@ -1,39 +1,134 @@
 import React from "react";
 import Call from "../../components/Call/Call.jsx";
 import styles from "../../styles/About.module.sass";
+import { motion, AnimatePresence } from "framer-motion";
 
 export default function About() {
+  let firstVar = {
+    hidden: {
+      x: -20,
+      opacity: 0,
+    },
+    visible: (num) => ({
+      x: 0,
+      opacity: 1,
+      transition: {
+        duration: 0.5,
+        delay: num * 0.1,
+      },
+    }),
+  };
+  let secondVar = {
+    hidden: {
+      x: 40,
+      opacity: 0,
+    },
+    visible: (num) => ({
+      x: 0,
+      opacity: 1,
+      transition: {
+        duration: 0.5,
+        delay: num * 0.1,
+      },
+    }),
+  };
+  let hiddenVar = {
+    hidden: {
+      opacity: 0,
+    },
+    visible: (num) => ({
+      opacity: 1,
+      transition: {
+        duration: 0.3,
+        delay: num * 0.1,
+      },
+    }),
+  };
   return (
     <div>
       <div className={styles.about}>
         <div className={styles.about_container}>
           <div className={styles.about_title}>
-            <h1>
-              Компания <span>Ислам Курулуш</span>
-            </h1>
-            <p>
+            <motion.h1
+              initial="hidden"
+              whileInView="visible"
+              viewport={{
+                once: true,
+                amount: 0.3,
+              }}
+              variants={firstVar}
+              custom={0}
+            >
+              Компания <h3>Ислам Курулуш</h3>
+            </motion.h1>
+            <motion.p
+              initial="hidden"
+              whileInView="visible"
+              viewport={{
+                once: true,
+                amount: 0.3,
+              }}
+              variants={secondVar}
+              custom={0}
+            >
               Компания "Ислам Курулуш" по праву занимает пьедестал молодого
               строительного творчества в Бишкеке.
-            </p>
-            <p>
+            </motion.p>
+            <motion.p
+              initial="hidden"
+              whileInView="visible"
+              viewport={{
+                once: true,
+                amount: 0.3,
+              }}
+              variants={secondVar}
+              custom={0}
+            >
               Пять лет на региональном рынке позволил пополнить активы компании
               сотнями крупных и средних проектов самого различного назначения
-            </p>
+            </motion.p>
           </div>
           <div className={styles.about_desc}>
-            <h2>Наш девиз</h2>
-            <p>
+            <motion.h2
+              initial="hidden"
+              whileInView="visible"
+              viewport={{
+                once: true,
+                amount: 0.3,
+              }}
+              variants={firstVar}
+              custom={0}
+            >Наш девиз</motion.h2>
+            <motion.p
+              initial="hidden"
+              whileInView="visible"
+              viewport={{
+                once: true,
+                amount: 0.3,
+              }}
+              variants={secondVar}
+              custom={0}
+            >
               Наш девиз - "Cоздание и мастерство на всех этапах строительных и
               монтажных работ, внимание к деталям и особым пожелания заказчиков.
               Умение совмещать несовместимое, находить и нестандартные и
               эффективные методы решения текущих задач, предлагать лучшие
               варианты реализации проектов в заданных условиях-умения которое
               дано не каждому"
-            </p>
+            </motion.p>
           </div>
 
           <div className={styles.about_desc}>
-            <h2>Наша миссия</h2>
+            <motion.h2
+              initial="hidden"
+              whileInView="visible"
+              viewport={{
+                once: true,
+                amount: 0.3,
+              }}
+              variants={firstVar}
+              custom={0}
+            >Наша миссия</motion.h2>
             <p>
               Миссия компания "Создание конечного строительного
               продукта,полностью отвечающего планам заказчика и его
@@ -42,14 +137,32 @@ export default function About() {
           </div>
 
           <div className={styles.about_desc}>
-            <h2>Как мы работаем</h2>
-            <p>
+            <motion.h2
+              initial="hidden"
+              whileInView="visible"
+              viewport={{
+                once: true,
+                amount: 0.3,
+              }}
+              variants={firstVar}
+              custom={0}
+            >Как мы работаем</motion.h2>
+            <motion.p
+              initial="hidden"
+              whileInView="visible"
+              viewport={{
+                once: true,
+                amount: 0.3,
+              }}
+              variants={secondVar}
+              custom={0}
+            >
               Созидаем, строим и реализуем проекты любой степени сложности с
               полной отдачей сил, мастерства и опыта. Все работы выполняются на
               совесть Соблюдение сроков и обязательств Профессиональный подход к
               делу и высокий уровень ответственности позволяют в итоге создать
               строительный продукт.
-            </p>
+            </motion.p>
           </div>
         </div>
       </div>
--- pages/Services/index.jsx
+++ pages/Services/index.jsx
@@ -1,38 +1,149 @@
 import React from 'react'
 import styles from "../../styles/Services.module.sass"
+import { motion, AnimatePresence } from "framer-motion";
 
 export default function index() {
+  let firstVar = {
+    hidden: {
+      x: -20,
+      opacity: 0,
+    },
+    visible: (num) => ({
+      x: 0,
+      opacity: 1,
+      transition: {
+        duration: 0.5,
+        delay: num * 0.1,
+      },
+    }),
+  };
+  let secondVar = {
+    hidden: {
+      x: 40,
+      opacity: 0,
+    },
+    visible: (num) => ({
+      x: 0,
+      opacity: 1,
+      transition: {
+        duration: 0.5,
+        delay: num * 0.1,
+      },
+    }),
+  };
+  let hiddenVar = {
+    hidden: {
+      opacity: 0,
+    },
+    visible: (num) => ({
+      opacity: 1,
+      transition: {
+        duration: 0.3,
+        delay: num * 0.1,
+      },
+    }),
+  };
   return (
     <div className={styles.container}>
-      <div className={styles.title}>
+      <motion.div
+        className={styles.title}
+        initial="hidden"
+        whileInView="visible"
+        viewport={{
+          once: true,
+          amount: 0.3,
+        }}
+        variants={firstVar}
+        custom={0}
+      >
         <h1 className={styles.title__srv}>Услуги</h1>
         <h1 className={styles.title__srv2}>компании</h1>
-      </div>
+      </motion.div>
       <div className={styles.card}>
-        <div className={styles.img}>
+        <motion.div
+          className={styles.img}
+          initial="hidden"
+          whileInView="visible"
+          viewport={{
+            once: true,
+            amount: 0.3,
+          }}
+          variants={secondVar}
+          custom={0}
+        >
           <img className={styles.image} src="images/img-servis.png" alt="" />
-        </div>
-        <div className={styles.info}>
+        </motion.div>
+        <motion.div
+          className={styles.info}
+          initial="hidden"
+          whileInView="visible"
+          viewport={{
+            once: true,
+            amount: 0.3,
+          }}
+          variants={hiddenVar}
+          custom={0}
+        >
           <p className={styles.info__srv}>Наш девиз - "Cоздание и мастерство на всех этапах строительных и монтажных работ, внимание к деталям и особым пожелания заказчиков. Умение совмещать несовместимое, находить и нестандартные и эффективные методы решения текущих задач, предлагать лучшие варианты реализации проектов в заданных условиях-умения которое дано не каждому"
           </p>
-        </div>
+        </motion.div>
       </div>
       <div className={`${styles.card} ${styles.card2}`}>
-        <div className={styles.info}>
+        <motion.div
+          className={styles.info}
+          initial="hidden"
+          whileInView="visible"
+          viewport={{
+            once: true,
+            amount: 0.3,
+          }}
+          variants={secondVar}
+          custom={0}
+        >
           <p className={`${styles.info__srv} ${styles.info__srv2}`}>Наш девиз - "Cоздание и мастерство на всех этапах строительных и монтажных работ, внимание к деталям и особым пожелания заказчиков. Умение совмещать несовместимое, находить и нестандартные и эффективные методы решения текущих задач, предлагать лучшие варианты реализации проектов в заданных условиях-умения которое дано не каждому"
           </p>
-        </div>
-        <div className={styles.img}>
+        </motion.div>
+        <motion.div
+          className={styles.img}
+          initial="hidden"
+          whileInView="visible"
+          viewport={{
+            once: true,
+            amount: 0.3,
+          }}
+          variants={firstVar}
+          custom={0}
+        >
           <img className={styles.image} src="images/img-servis.png" alt="" />
-        </div>
+        </motion.div>
       </div>
       <div className={styles.card}>
-        <div className={styles.img}>
+        <motion.div
+          className={styles.img}
+          initial="hidden"
+          whileInView="visible"
+          viewport={{
+            once: true,
+            amount: 0.3,
+          }}
+          variants={firstVar}
+          custom={0}
+        >
           <img className={styles.image} src="images/img-servis.png" alt="" />
-        </div>
-        <div className={styles.info}>
+        </motion.div>
+        <motion.div
+          className={styles.info}
+          initial="hidden"
+          whileInView="visible"
+          viewport={{
+            once: true,
+            amount: 0.3,
+          }}
+          variants={secondVar}
+          custom={0}
+        >
           <p className={styles.info__srv}>Наш девиз - "Cоздание и мастерство на всех этапах строительных и монтажных работ, внимание к деталям и особым пожелания заказчиков. Умение совмещать несовместимое, находить и нестандартные и эффективные методы решения текущих задач, предлагать лучшие варианты реализации проектов в заданных условиях-умения которое дано не каждому"</p>
-        </div>
+        </motion.div>
       </div>
     </div>
   )
